f24e4d2bfb69980799584c828a7307c4a75c2845
dethrtrns <rajshekharsingh060593@gmail.com>
2026-02-04 23:57:30 +0530

    video ui update

--- data/pages.ts
+++ data/pages.ts
@@ -16,7 +16,7 @@ export const pagesData = {
     videoTitle: "Our Journey",
     videoSubtitle: "A glimpse into our world of precision manufacturing.",
     videoSource: "/videos/our-journey.mp4",
-    videoThumbnail: "",
+    videoThumbnail: "/videos/our-journey-thumb-2.webp",
     factoryImage: "/real-assets/building-our-plant-side-view.webp"
   },
   facility: {
